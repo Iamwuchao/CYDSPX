db6d1c61367bb91597cd2f7d67c6555dec77f30e
WuChao <wuchao155425@gmail.com>
2016-11-23 16:37:45 +0800

    专家登录后跳转页面

---
+++ CYDSPX/src/main/java/cydspx/pagecontroller/ExpertPageController.java
@@ -0,0 +1,19 @@
+package cydspx.pagecontroller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cydspx.globalInfo.SessionKey;
+import cydspx.globalInfo.UserType;
+import cydspx.mode.User;
+
+@Controller
+public class ExpertPageController {
+	
+	
+	
+	
+	
+}
--- CYDSPX/src/main/java/cydspx/pagecontroller/PageController.java
+++ CYDSPX/src/main/java/cydspx/pagecontroller/PageController.java
@@ -1,7 +1,13 @@
 package cydspx.pagecontroller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import cydspx.globalInfo.SessionKey;
+import cydspx.globalInfo.UserType;
+import cydspx.mode.User;
 
 @Controller
 public class PageController {
@@ -9,5 +15,23 @@ public class PageController {
 	@RequestMapping("/cydspx")
 	public String loginPage(){
 		return "login";
+	}
+	
+	@RequestMapping("/cydspx/homepage")
+	public String showHometpage(HttpSession session){
+		User user = (User) session.getAttribute(SessionKey.USER_INFO.name());
+		if(user.getUserType()==UserType.EXPERT.ordinal()){
+			return "/expert/expert";
+		}
+		else if(user.getUserType()==UserType.SCHOOLADMIN.ordinal()){
+			return "";
+		}
+		else if(user.getUserType()==UserType.SUPERADMIN.ordinal()){
+			return "";
+		}
+		else{
+			return "/cydspx";
+		}
+		
 	}
 }
