c2bb2036fe1a7330f8d1d975dc733c66486df4ab
WuChao <wuchao155425@gmail.com>
2016-11-24 09:39:04 +0800

    消除一个bug

---
+++ CYDSPX/src/main/java/cydspx/mode/CandidateAbstract.java
@@ -0,0 +1,14 @@
+package cydspx.mode;
+
+import lombok.Data;
+
+@Data
+public class CandidateAbstract {
+	private int id;//id
+	private String name;//姓名
+	private String workunit;//工作单位
+	private String photograph;
+	private String job;					//职务			17
+	private String title;				//职称			18
+	private String resume;//简历
+}
