0bd7367952fb1f402847014825a0f3eb6e5f3fa6
WuChao <wuchao155425@gmail.com>
2016-11-25 13:57:05 +0800

    修改了UserHandler的一个注解错误

--- CYDSPX/src/main/java/cydspx/handler/UserHandler.java
+++ CYDSPX/src/main/java/cydspx/handler/UserHandler.java
@@ -1,6 +1,7 @@
 package cydspx.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import cydspx.dbserver.UserDBServer;
 import cydspx.mode.User;
@@ -8,6 +9,7 @@ import cydspx.mode.User;
 /*
  * 该类用于处理一些共用的用户请求
  */
+@Component
 public class UserHandler {
 	
 	@Autowired
