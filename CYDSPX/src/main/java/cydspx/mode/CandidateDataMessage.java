cfc8d0c731f41aad448586ac811101ee74c72dcf
WuChao <wuchao155425@gmail.com>
2016-12-08 09:57:36 +0800

    asdasdA

---
+++ CYDSPX/src/main/java/cydspx/mode/CandidateDataMessage.java
@@ -0,0 +1,11 @@
+package cydspx.mode;
+
+import java.util.List;
+
+public class CandidateDataMessage {
+	public Candidate candidate;
+	public List<ElectJoin> electjoins;
+	public List<Prize> prizes;
+	public List<String> services;
+	public List<String> vocations;
+}
